00358c133a8175fde2ac19f82ea6c6d7873eabe4
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2020-06-26 00:06:40 +0200

    ANG: Finalize exercise project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'exercise';
+  selected = undefined;
+  title = 'Movie Flops';
+  data = [
+    {
+      id: 1,
+      title: 'Disaster Movie',
+      imageUrl:
+        'https://m.media-amazon.com/images/M/MV5BMTIzMDQyNDgwNl5BMl5BanBnXkFtZTcwMDA0MTc3MQ@@._V1_.jpg',
+      rating: 2,
+    },
+    {
+      id: 2,
+      title: 'Jack and Jill',
+      imageUrl:
+        'https://m.media-amazon.com/images/M/MV5BNjczMTU5OTUyMl5BMl5BanBnXkFtZTcwODEzNjc3Ng@@._V1_SY1000_CR0,0,674,1000_AL_.jpg',
+      rating: 3,
+    },
+    {
+      id: 3,
+      title: 'Blood Money',
+      imageUrl:
+        'https://m.media-amazon.com/images/M/MV5BMTU3Nzk5MDQxMl5BMl5BanBnXkFtZTcwNjI0NDkwOA@@._V1_SY1000_CR0,0,708,1000_AL_.jpg',
+      rating: 3,
+    },
+  ];
+  toggleSelect(movieId: number) {
+    this.selected = this.selected === movieId ? undefined : movieId;
+  }
+  isSelected(movieId: number): boolean {
+    return this.selected === movieId;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
